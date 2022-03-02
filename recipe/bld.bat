fab5a742eab4ed6c0d4978f747c8d17a484c03dc
Scott Bailey <sbailey@anaconda.com>
2022-03-02 10:18:28 -0700

    Prefer Ninja.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,35 @@
-REM Install headers with projectConfig.cmake files with cmake
+:: cmd
 
-mkdir build
-cd build
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D PYBIND11_TEST=OFF ^
-      %SRC_DIR%
+:: Isolate the build.
+mkdir Build
+cd Build
 if errorlevel 1 exit 1
 
-nmake
+:: Generate the build files.
+cmake %SRC_DIR% -G"Ninja" %CMAKE_ARGS% ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DPYBIND11_TEST=OFF ^
+      -DCMAKE_BUILD_TYPE=Release
+
+
+:: Build and install.
+ninja install
 if errorlevel 1 exit 1
 
-nmake install
-if errorlevel 1 exit 1
 
-REM Install Python package
+:: Perforem tests.
+::  ninja test
+::  path_to\test
+::  if errorlevel 1 exit 1
+
+
+:: Install Python package
 set PYBIND11_USE_CMAKE=1
 cd %SRC_DIR%
 python %SRC_DIR%\setup.py install --single-version-externally-managed --record record.txt
+if errorlevel 1 exit 1
+
+
+:: Error free exit.
+exit 0
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,23 @@
-# Install headers with projectConfig.cmake files with cmake
-mkdir build
-cd build
+#!/bin/bash
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DPYTHON_EXECUTABLE=$PREFIX/bin/python \
-  -DPYBIND11_TEST=OFF \
-  $SRC_DIR
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
 
-make
-make install
+# Generate the build files.
+cmake ${SRC_DIR} -G"Ninja" ${CMAKE_ARGS} \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DPYTHON_EXECUTABLE=$PREFIX/bin/python \
+      -DPYBIND11_TEST=OFF \
+      -DCMAKE_BUILD_TYPE=Release
+
+# Build and install.
+ninja install || exit 1
+
+# Perform tests.
+#  ninja test || exit 1
+#  path_to/test || exit 1
 
 # Install Python package
 export PYBIND11_USE_CMAKE=1
