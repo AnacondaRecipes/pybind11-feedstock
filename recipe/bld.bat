c5d12857164479f142b2b5998d529f1a25f047e4
Scott B <Scott@TheBaileyFamily.org>
2022-03-03 08:43:13 -0700

    Update to 2.9.1
    Changes:
    - Update version from `2.8.1` to `2.9.1`.
    - Prefer ninja to NMake + make.
    - Enable author's tests, but do not act on results.
    - Disable win32 as it's a deprecated platform.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,41 @@
-REM Install headers with projectConfig.cmake files with cmake
+:: cmd
 
-mkdir build
-cd build
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D PYBIND11_TEST=OFF ^
-      %SRC_DIR%
+:: Isolate the build.
+mkdir Build
+cd Build
 if errorlevel 1 exit 1
 
-nmake
+
+:: Generate the build files.
+cmake %SRC_DIR% -G"Ninja" %CMAKE_ARGS% ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DPYBIND11_TEST=ON ^
+      -DCMAKE_BUILD_TYPE=Release
+
+
+:: Build.
+ninja
 if errorlevel 1 exit 1
 
-nmake install
+
+:: Perforem tests.
+ninja check
+::  if errorlevel 1 exit 1
+
+
+:: Install.
+ninja install
 if errorlevel 1 exit 1
 
-REM Install Python package
+
+:: Install Python package
 set PYBIND11_USE_CMAKE=1
+set CMAKE_GENERATOR=Ninja
 cd %SRC_DIR%
-python %SRC_DIR%\setup.py install --single-version-externally-managed --record record.txt
+%PYTHON% %SRC_DIR%\setup.py install --single-version-externally-managed --record record.txt
+if errorlevel 1 exit 1
+
+
+:: Error free exit.
+exit 0
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,33 @@
-# Install headers with projectConfig.cmake files with cmake
-mkdir build
-cd build
+#!/bin/bash
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DPYTHON_EXECUTABLE=$PREFIX/bin/python \
-  -DPYBIND11_TEST=OFF \
-  $SRC_DIR
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
 
-make
-make install
+
+# Generate the build files.
+cmake ${SRC_DIR} -G"Ninja" ${CMAKE_ARGS} \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DPYTHON_EXECUTABLE=$PYTHON \
+      -DPYBIND11_TEST=ON \
+      -DCMAKE_BUILD_TYPE=Release
+
+
+# Build.
+ninja || exit 1
+
+# Perform tests.
+#ninja check || exit 1
+ninja check || echo "======================= Testing Failed =============================" # Keep testing active for now and fix as time allows.
+
+
+# Install.
+ninja install || exit 1
+
 
 # Install Python package
 export PYBIND11_USE_CMAKE=1
+export CMAKE_GENERATOR=Ninja
 cd $SRC_DIR
-python $SRC_DIR/setup.py install --single-version-externally-managed --record record.txt
+$PYTHON $SRC_DIR/setup.py install --single-version-externally-managed --record record.txt
