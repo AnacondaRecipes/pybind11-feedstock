f80ca42ed7dae8de319c9a16fe8f62f328fa21ba
Eric Lundby <Eric.Lundby@gmail.com>
2024-10-29 14:54:42 -0600

    Revert "Fix Windows build."
    This reverts commit b912608d8e0cb8a29ec1d4628d6edfe94e184247.

--- recipe/build-pybind11-global.bat
+++ recipe/build-pybind11-global.bat
@@ -2,7 +2,5 @@
 :: https://github.com/conda/conda-build/blob/d75ef9c66dbcc832d8d96f33a2aecfe893266e8c/conda_build/build.py#L2517-L2527
 set PYBIND11_GLOBAL_SDIST=1
 set PYBIND11_GLOBAL_PREFIX=Library
-mkdir build
-cmake -G "Visual Studio 16 2019" -S . -B ./build -DBUILD_TESTING=OFF -DPYBIND11_NOPYTHON=ON -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
-cmake --install build
+%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 if errorlevel 1 exit 1
--- recipe/build-pybind11.bat
+++ recipe/build-pybind11.bat
@@ -1,4 +1,4 @@
-mkdir build
-cmake -G "Visual Studio 16 2019" -S . -B ./build -DBUILD_TESTING=OFF -DPYBIND11_NOPYTHON=ON -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
-cmake --install build
+:: There are several required flags, like --no-deps, but conda-build nicely sets them for us
+:: https://github.com/conda/conda-build/blob/d75ef9c66dbcc832d8d96f33a2aecfe893266e8c/conda_build/build.py#L2517-L2527
+%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 if errorlevel 1 exit 1
