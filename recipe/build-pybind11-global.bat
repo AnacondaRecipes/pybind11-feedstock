22292b780787c07f6daec9dd5ec386541d34bfef
Eric Lundby <Eric.Lundby@gmail.com>
2024-10-30 13:53:31 -0600

    Somewhat closer on windows?

--- recipe/build-pybind11-global.bat
+++ recipe/build-pybind11-global.bat
@@ -2,5 +2,14 @@
 :: https://github.com/conda/conda-build/blob/d75ef9c66dbcc832d8d96f33a2aecfe893266e8c/conda_build/build.py#L2517-L2527
 set PYBIND11_GLOBAL_SDIST=1
 set PYBIND11_GLOBAL_PREFIX=Library
-%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
+del build 2>null
+
+cmake -G "Visual Studio 16 2019" -S . -B ./build -DBUILD_TESTING=OFF -DPYBIND11_NOPYTHON=ON^
+  -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%"      ^
+  -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
+  -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"    ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+
+cmake --install build
 if errorlevel 1 exit 1
