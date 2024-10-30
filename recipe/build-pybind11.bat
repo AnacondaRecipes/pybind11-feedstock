0ebd88249a48fa0ed466cb2ba992e39e833e24c9
Eric Lundby <Eric.Lundby@gmail.com>
2024-10-30 16:16:26 -0600

    revert to pip install.

--- recipe/build-pybind11.bat
+++ recipe/build-pybind11.bat
@@ -1,11 +1,4 @@
-rmdir /S /Q build
-
-cmake -G "Visual Studio 16 2019" -S . -B ./build -DBUILD_TESTING=OFF -DPYBIND11_NOPYTHON=ON^
-  -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%"      ^
-  -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
-  -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
-  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"    ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
-
-cmake --install build
+:: There are several required flags, like --no-deps, but conda-build nicely sets them for us
+:: https://github.com/conda/conda-build/blob/d75ef9c66dbcc832d8d96f33a2aecfe893266e8c/conda_build/build.py#L2517-L2527
+%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 if errorlevel 1 exit 1
